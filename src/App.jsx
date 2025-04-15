43d286f94fef70dbbb3864fe4d0579d2578902c8
felix-op <felixoscarperezsegovia@gmail.com>
2025-04-15 13:17:53 -0300

    configuracion y header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// Como importar imagenes de diferentes lugares
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import "./App.css";
+import { Index } from "./pages";
+
+function App() {
+  return <Index />;
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,64 @@
+import EscudoLogo from "./../assets/escudo-u.png";
+
+export function Index() {
+  return (
+    <>
+      <header className="flex justify-end items-center bg-linear-to-r from-azul-oscuro to-verde-oscuro py-[20px] pr-[20px]">
+        <nav className="mr-[20px]">
+          <ul className="flex gap-4">
+            <Link href="#">Juega y Diviértete</Link>
+            <Link href="#">Aprende y Entrena</Link>
+            <Link href="#">Descubre Historias</Link>
+            <Link href="#">Aporta al Equipo</Link>
+          </ul>
+        </nav>
+        <Escudo />
+      </header>
+      <main>
+        <Presentacion />
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+function Presentacion() {
+  return (
+    <section className="grid grid-cols-7 grid-rows-2 gap-10 bg-red-200 h-100 p-[20px]">
+      <Imagen src="#" alt="Ilustrativa" rows="1" columns="2" />
+      <Imagen src="#" alt="Ilustrativa" rows="2" columns="1" />
+      <Imagen src="#" alt="Ilustrativa" rows="1" columns="1" />
+      <Imagen src="#" alt="Ilustrativa" rows="1" columns="2" />
+      <Imagen src="#" alt="Ilustrativa" rows="2" columns="1" />
+      <Imagen src="#" alt="Ilustrativa" rows="1" columns="2" />
+      <Imagen src="#" alt="Ilustrativa" rows="1" columns="3" />
+    </section>
+  );
+}
+
+function Imagen({ src, alt, link, rows, columns }) {
+  const clases = [
+    "bg-black",
+    columns && `col-span-[${columns}]`,
+    rows && `row-span-[${rows}]`,
+  ]
+    .filter(Boolean)
+    .join(" ");
+
+  const image = <img src={src} alt={alt} className={clases} />;
+
+  return link ? <a href={link}>{image}</a> : image;
+}
+
+function Link({ href, children }) {
+  return (
+    <li className="text-letra hover:bg-verde-lima px-3 py-2 rounded">
+      <a href={href}>{children}</a>
+    </li>
+  );
+}
+
+function Escudo() {
+  return <img src={EscudoLogo} className="w-45 h-auto" />;
+}
